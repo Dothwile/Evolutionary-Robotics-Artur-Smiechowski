704def99a5477817056a1aa8aeea70a69fbd14d7
Artur Smiechowski <35614427+Dothwile@users.noreply.github.com>
2021-02-14 16:26:56 -0500

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -1,7 +1,13 @@
 #simulate.py
 #Created by Artur Smiechowski 14/02/2021
 import pybullet as p
+import time
 
-physicsClient = p.connect(p.GUI)
-p.disconnect()
+physicsClient = p.connect(p.GUI) #Create Physics Client
 
+for step in range(1000):
+    #Loop step times
+    p.stepSimulation()
+    time.sleep(1/60)
+
+p.disconnect() #Disconnect from Physics client
