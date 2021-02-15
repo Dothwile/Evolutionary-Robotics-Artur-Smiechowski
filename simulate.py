a8cf834b16e64dd853905f0c5f3d63073c4bdcea
Artur Smiechowski <35614427+Dothwile@users.noreply.github.com>
2021-02-15 00:08:56 -0500

    Adding World Complexity
    Added gravity and floor plane as well as supporting code to pull necessary data from pybullet_data

--- simulate.py
+++ simulate.py
@@ -1,9 +1,18 @@
 #simulate.py
 #Created by Artur Smiechowski 14/02/2021
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI) #Create Physics Client
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+#Set World Parameters
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+
+#Load World
+p.loadSDF("box.sdf")
 
 for step in range(1000):
     #Loop step times
