da1f1dfc1caca02c4e0804149c9e403a3ebbb521
Artur Smiechowski <art27833@gmail.com>
2021-02-15 20:44:51 -0500

    Fixed Generate
    For some reason somewhere in commit generate.py got cleared, redidthe internal code

--- generate.py
+++ generate.py
@@ -1,2 +1,10 @@
 #Generate.py
 #Created by Artur Smiechowski 14/02/2021
+import pyrosim.pyrosim as pyrosim
+import pybullet
+
+pyrosim.Start_SDF("box.sdf")
+
+pyrosim.Send_Cube(name="Box",pos=[0,0,0.5],size=[1,1,1])
+
+pyrosim.End()
