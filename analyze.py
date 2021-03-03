f846120acd10a562daf10664257c659a9c3f378a
Artur Smiechowski <art27833@gmail.com>
2021-03-03 15:34:35 -0500

    Finished Motors
    Did it all in one go

--- analyze.py
+++ analyze.py
@@ -4,10 +4,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 'r' added in front of path to avoid triiping formatting issues from \
+'''
 backLegSensorValues = np.load(r'data\SensorDat1.npy')
 frontLegSensorValues = np.load(r'data\frontLegSensorValues.npy')
 
 plt.plot(backLegSensorValues,label='back_leg',linewidth=3)
 plt.plot(frontLegSensorValues,label='front_leg')
+plt.legend()
+plt.show()
+'''
+'''
+targetAngles = np.load(r'data\targetAngles.npy')
+
+plt.plot(np.linspace(0,1000, 1000),targetAngles,label='Target Angles')
+plt.xlabel('Angle [rad]')
+plt.ylabel('sin(x)')
+plt.legend()
+plt.show()
+'''
+f_targetAngles = np.load(r'data\f_targetAngles.npy')
+b_targetAngles = np.load(r'data\b_targetAngles.npy')
+
+
+plt.plot(np.linspace(0,1000, 1000),f_targetAngles,label='Front Leg Target Angles')
+plt.plot(np.linspace(0,1000, 1000),b_targetAngles,label='Back Leg Target Angles')
+plt.xlabel('Angle [rad]')
+plt.ylabel('sin(x)')
 plt.legend()
 plt.show()
--- simulate.py
+++ simulate.py
@@ -5,6 +5,7 @@ import pybullet_data
 import time
 import pyrosim.pyrosim as pyrosim
 import numpy as np
+import random
 
 physicsClient = p.connect(p.GUI) #Create Physics Client
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -14,27 +15,51 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 
+steps = 1000
+
+f_amplitude = np.pi/3
+f_frequency = 1/25
+f_phaseOffset = 0
+
+f_targetAngles = f_amplitude*np.sin(f_frequency*np.linspace(0,1000,1000) + f_phaseOffset)
+
+b_amplitude = np.pi/6
+b_frequency = 1/25
+b_phaseOffset = np.pi/4
+
+b_targetAngles = b_amplitude*np.sin(b_frequency*np.linspace(0,1000,1000) + b_phaseOffset)
+
 #Load World
 p.loadSDF("box.sdf")
 
 pyrosim.Prepare_To_Simulate("body.urdf")
 
-backLegSensorValues = np.zeros(100)
-frontLegSensorValues = np.zeros(100)
+backLegSensorValues = np.zeros(steps)
+frontLegSensorValues = np.zeros(steps)
 print(backLegSensorValues)
 print(frontLegSensorValues)
 
-for step in range(100):
+for step in range(steps):
     #Loop step times
     p.stepSimulation()
     backLegSensorValues[step] = pyrosim.Get_Touch_Sensor_Value_For_Link("Back_Leg")
     frontLegSensorValues[step] = pyrosim.Get_Touch_Sensor_Value_For_Link("Front_Leg")
+
+    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId,jointName="Torso_Leg_Back",controlMode=p.POSITION_CONTROL,targetPosition=b_targetAngles[step],maxForce=500)
+    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId,jointName="Torso_Leg_Front",controlMode=p.POSITION_CONTROL,targetPosition=f_targetAngles[step],maxForce=500)
+    
     time.sleep(1/60)
 
 p.disconnect() #Disconnect from Physics client
 
 # \ in filepath names trip formatting issues, thus the leading 'r' to declare literal
-np.save(r'data\SensorDat1',backLegSensorValues)
+#np.save(r'data\SensorDat1',backLegSensorValues)
 np.save(r'data\frontLegSensorValues',frontLegSensorValues)
+
+#np.save(r'data\targetAngles',targetAngles)
+
+#np.save(r'data\f_targetAngles',f_targetAngles)
+#np.save(r'data\b_targetAngles',b_targetAngles)
+
 print(backLegSensorValues)
 print(frontLegSensorValues)
