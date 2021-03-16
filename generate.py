1765f3016a36a160e5c854f9a2148e56ebb0a52e
Artur Smiechowski <art27833@gmail.com>
2021-03-15 21:31:04 -0400

    Generate Added NN
    ...

--- generate.py
+++ generate.py
@@ -6,30 +6,34 @@ import pybullet
 length = 1
 width = 1
 height = 1
-rx = 0
-ry = 0
-rz = 0.5
 
-wx = 3
-wy = 3
-wz = 0.5
+x = 0
+y = 0
+z = 0.5
 
-def Create_World():
+def Generate_World():
     pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Box",pos=[wx,wy,wz],size=[length,width,height])
+    pyrosim.Send_Cube(name="Box", pos=[x-5,y+5,z] , size=[length,width,height])
     pyrosim.End()
 
-def Create_Robot():
+def Generate_Body():
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Torso",pos=[rx,ry,rz+1],size=[length,width,height])
-    pyrosim.Send_Joint(name="Torso_Leg_Front",parent="Torso",child="Front_Leg",type="revolute",position= "0.5 0 1")
-    pyrosim.Send_Cube(name="Front_Leg",pos=[rx+0.5,ry,rz-1],size=[length,width,height])
-    pyrosim.Send_Joint(name="Torso_Leg_Back",parent="Torso",child="Back_Leg",type="revolute",position= "-0.5 0 1")
-    pyrosim.Send_Cube(name="Back_Leg",pos=[rx-0.5,ry,rz-1],size=[length,width,height])
+    pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5] , size=[length,width,height])
+    pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = "2 0 1")
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5] , size=[length,width,height])
+    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = "1 0 1")
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5] , size=[length,width,height])
+    pyrosim.End()
+    
+def Generate_Brain():
+    pyrosim.Start_NeuralNetwork("brain.nndf")
+    pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
+    pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "BackLeg")
+    pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
+    pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
+    pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
     pyrosim.End()
 
-
-Create_World()
-print("Created world")
-Create_Robot()
-print("Created robot")
+Generate_World()
+Generate_Body()
+Generate_Brain()
