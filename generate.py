f92accf1e33f84a98bff8bbeb60ed6a1a5ac8abb
Artur Smiechowski <35614427+Dothwile@users.noreply.github.com>
2021-03-21 19:59:12 -0400

    Finished Synapses
    Added synapses and weights to walk backwards

--- generate.py
+++ generate.py
@@ -38,8 +38,11 @@ def Generate_Brain():
     
     # Synapses
     
-    pyrosim.Send_Synapse(sourceNeuronName = 0, targetNeuronName = 3, weight = 1.0)
-    pyrosim.Send_Synapse(sourceNeuronName = 1, targetNeuronName = 3, weight = 1.0)
+    pyrosim.Send_Synapse(sourceNeuronName = 0, targetNeuronName = 3, weight = 0.1)
+    pyrosim.Send_Synapse(sourceNeuronName = 1, targetNeuronName = 3, weight = 1.5)
+    pyrosim.Send_Synapse(sourceNeuronName = 1, targetNeuronName = 4, weight = 1.0)
+    pyrosim.Send_Synapse(sourceNeuronName = 2, targetNeuronName = 4, weight = 0.3)
+    pyrosim.Send_Synapse(sourceNeuronName = 0, targetNeuronName = 4, weight = 0.1)
     
     pyrosim.End()
 
