d4cd80d2c9de8f0df47c45853de08e4fa48fdad2
Artur Smiechowski <art27833@gmail.com>
2021-04-19 22:04:34 -0400

    Revert "Final Start"
    This reverts commit 4f5a0d8ddcdd33502361fc34fe0df7fe41b1282a.

--- simulation.py
+++ simulation.py
@@ -25,7 +25,7 @@ class SIMULATION:
         p.setGravity(0,0,c.g)
 
         # Create a World object
-        self.world = WORLD(solutionID)
+        self.world = WORLD()
         # Create a Robot object
         self.robot = ROBOT(solutionID)
         
--- world.py
+++ world.py
@@ -5,8 +5,8 @@ import pybullet as p
 
 class WORLD:
     
-    def __init__(self, solutionID):
-        p.loadSDF("world" + str(solutionID) + ".sdf")
+    def __init__(self):
+        p.loadSDF("world.sdf")
         #print("Loaded world")
         self.planeId = p.loadURDF("plane.urdf")
         #print("Loaded plane")
